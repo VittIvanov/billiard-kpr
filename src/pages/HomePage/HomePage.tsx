dd08c1f2940fb906ec3bc6e41fda9cf32fe8141f
VittIvanov <ivanov.vitaly.wdm@mail.ru>
2024-03-21 21:41:41 +0600

    add Homepage

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { HeaderWrapper } from "./styled"
+
+
+const HomePage: React.FC = () => {
+  return (
+    <HeaderWrapper>
+      <h1>«BILLIARD»</h1>
+    </HeaderWrapper>
+  )
+}
+
+export default HomePage
---
+++ src/pages/HomePage/styled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"
+
+export const HeaderWrapper = styled.div`
+background-color: #4b3a3a;
+height: 5vh;
+text-align: center;
+color: bisque;
+`
+
+// export { }
